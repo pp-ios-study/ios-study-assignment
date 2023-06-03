2cc89ea351f6dbd76c1af1635147a5abc6493066
최승명 <710csm@naver.com>
2023-06-04 00:24:56 +0900

    feat: 검색화면 Coordinator 추가

--- ios-study-assignment/ios-study-assignment/Sources/Main/Search/SearchCoordinator.swift
+++ ios-study-assignment/ios-study-assignment/Sources/Main/Search/SearchCoordinator.swift
@@ -5,10 +5,22 @@
 //  Created by 최승명 on 2023/05/27.
 //
 
-import Foundation
+import UIKit
 
 import Common
+import RxSwift
+import RxCocoa
 
 class SearchCoordinator: BaseCoordinator {
-    
+
+    private let disposeBag = DisposeBag()
+
+    override init() { }
+
+    override func start() {
+        let viewController = SearchViewController()
+
+        navigationController.isNavigationBarHidden = false
+        navigationController.viewControllers = [viewController]
+    }
 }
