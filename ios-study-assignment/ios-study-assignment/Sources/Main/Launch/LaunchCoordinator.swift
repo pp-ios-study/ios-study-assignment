a221f59cea32411ecbae3c04c303c0daf1837820
최승명 <710csm@naver.com>
2023-06-04 00:17:50 +0900

    feat: 시작화면 Coordinator 추가

--- ios-study-assignment/ios-study-assignment/Sources/Main/Launch/LaunchCoordinator.swift
+++ ios-study-assignment/ios-study-assignment/Sources/Main/Launch/LaunchCoordinator.swift
@@ -5,10 +5,22 @@
 //  Created by 최승명 on 2023/05/27.
 //
 
-import Foundation
+import UIKit
 
 import Common
+import RxSwift
+import RxCocoa
 
 class LaunchCoordinator: BaseCoordinator {
-    
+
+    private let disposeBag = DisposeBag()
+
+    override init() { }
+
+    override func start() {
+        let viewController = LaunchViewController()
+
+        navigationController.isNavigationBarHidden = true
+        navigationController.viewControllers = [viewController]
+    }
 }
